9cda975a20b7592aea11101e9911ba50d9585a87
Mark Anderson <marksmanderson@gmail.com>
2017-11-20 11:38:40 -0800

    Added Naive Bayes code.

---
+++ naive_bayes/nb_test.py
@@ -0,0 +1,87 @@
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+import re
+from math import log
+
+# define for string cleaning
+strip_special_chars = re.compile("[^A-Za-z0-9 ]+")
+def cleanSentences(string):
+    string = string.lower().replace("<br />", " ")
+    return re.sub(strip_special_chars, "", string.lower())
+
+# define for index finding
+def findIndex(search_list, begin, end, key):
+    #print 'Begin: ' + str(begin) + ' End: ' + str(end)
+    mid = int((end - begin + 1)/2) + begin
+    if end == begin:
+        if search_list[mid] == key:
+            return mid
+        else:
+            return -1
+    if end == begin + 1:
+        if search_list[begin] == key:
+            return begin
+        if search_list[end] == key:
+            return end
+        else:
+            return -1
+    if search_list[mid] < key:
+        return findIndex(search_list, mid, end, key)
+    return findIndex(search_list, begin, mid, key)
+
+nb_neg_weights = open("model_data/negative_weights.txt", "r")
+nb_pos_weights = open("model_data/positive_weights.txt", "r")
+nb_stats       = open("model_data/training_stats.txt", "r")
+wordsList = []
+positive_weights = []
+negative_weights = []
+for i in range (0, int(nb_stats.readline())):
+    wordsList.append(nb_stats.readline().replace('\n', ''))
+    positive_weights.append(float(nb_pos_weights.readline()))
+    negative_weights.append(float(nb_neg_weights.readline()))
+#print wordsList
+#print positive_weights
+#print negative_weights
+nb_stats.close()
+nb_neg_weights.close()
+nb_pos_weights.close()
+
+positiveFiles = ['training_data/positiveReviews/' + f for f in listdir('training_data/positiveReviews') if isfile(join('training_data/positiveReviews/', f))]
+negativeFiles = ['training_data/negativeReviews/' + f for f in listdir('training_data/negativeReviews') if isfile(join('training_data/negativeReviews/', f))]
+
+total_files = 0
+incorrect   = 0
+for pf in positiveFiles:
+    total_files += 1
+    positive_total = 0
+    negative_total = 0
+    with open(pf, "r") as f:
+        line = cleanSentences(f.readline())
+        for word in line.split():
+            index = findIndex(wordsList, 0, len(wordsList)-1, word)
+            if not index == -1:
+                positive_total += log(positive_weights[index])
+                negative_total += log(negative_weights[index])
+        if positive_total <= negative_total:
+            incorrect += 1
+print ('Positive files finished')
+
+for nf in negativeFiles:
+    total_files += 1
+    positive_total = 0
+    negative_total = 0
+    with open(nf, "r") as f:
+        line = cleanSentences(f.readline())
+        for word in line.split():
+            index = findIndex(wordsList, 0, len(wordsList)-1, word)
+            if not index == -1:
+                positive_total += log(positive_weights[index])
+                negative_total += log(negative_weights[index])
+        if negative_total <= positive_total:
+            incorrect += 1
+print ('Negative files finished')
+
+print 'Total files: ' + str(total_files)
+print 'Incorrect classifications: ' + str(incorrect)
+print 'Correctness: ' + str(100 - ((float(incorrect)/float(total_files))*100)) + '%'
