0918e8c86c127ddc60d39e40b2aa1ee20b31ce54
Mark Anderson <marksmanderson@gmail.com>
2017-11-21 07:21:29 -0800

    Added option to test on adversarial input. Added handcrafed examples.

--- naive_bayes/README.txt
+++ naive_bayes/README.txt
@@ -1,4 +1,5 @@
 To train model, run "python nb_train.py"
 To test model, run "python nb_test.py"
+To test model on adversarial input, run "python nb_test.py --adversarial"
 
 No need to train model, though, because model weights are stored in model_data folder.
--- naive_bayes/nb_test.py
+++ naive_bayes/nb_test.py
@@ -2,6 +2,7 @@ import numpy as np
 from os import listdir
 from os.path import isfile, join
 import re
+import sys
 
 # define for string cleaning
 strip_special_chars = re.compile("[^A-Za-z0-9 ]+")
@@ -28,6 +29,10 @@ def findIndex(search_list, begin, end, key):
         return findIndex(search_list, mid, end, key)
     return findIndex(search_list, begin, mid, key)
 
+adversarial_input = False
+if ('--adversarial' in sys.argv):
+    adversarial_input = True
+
 # read in trained model
 nb_neg_weights = open("model_data/negative_weights.txt", "r")
 nb_pos_weights = open("model_data/positive_weights.txt", "r")
@@ -43,9 +48,14 @@ nb_stats.close()
 nb_neg_weights.close()
 nb_pos_weights.close()
 
-# test on test data
-positiveFiles = ['../training_data/positiveReviews_test/' + f for f in listdir('../training_data/positiveReviews_test') if isfile(join('../training_data/positiveReviews_test/', f))]
-negativeFiles = ['../training_data/negativeReviews_test/' + f for f in listdir('../training_data/negativeReviews_test') if isfile(join('../training_data/negativeReviews_test/', f))]
+# test on test data (not adversarial or adversarial)
+if not adversarial_input:
+    positiveFiles = ['../training_data/positiveReviews_test/' + f for f in listdir('../training_data/positiveReviews_test') if isfile(join('../training_data/positiveReviews_test/', f))]
+    negativeFiles = ['../training_data/negativeReviews_test/' + f for f in listdir('../training_data/negativeReviews_test') if isfile(join('../training_data/negativeReviews_test/', f))]
+else:
+    positiveFiles = ['../adversarial_data/positiveReviews_test/' + f for f in listdir('../adversarial_data/positiveReviews_test') if isfile(join('../adversarial_data/positiveReviews_test/', f))]
+    negativeFiles = ['../adversarial_data/negativeReviews_test/' + f for f in listdir('../adversarial_data/negativeReviews_test') if isfile(join('../adversarial_data/negativeReviews_test/', f))]
+
 total_files = 0
 incorrect   = 0
 for pf in positiveFiles:
