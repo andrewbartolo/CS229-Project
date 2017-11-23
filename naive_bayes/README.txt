af684139b40b82165e4d6e0f9c2c95906fbbddde
Mark Anderson <marksmanderson@gmail.com>
2017-11-22 17:59:45 -0800

    Coded basic naive-Bayes-targeted adversary.

--- naive_bayes/README.txt
+++ naive_bayes/README.txt
@@ -1,6 +1,10 @@
 To train model, run "python nb_train.py"
 To test model, run "python nb_test.py"
-To test model on adversarial input, run "python nb_test.py --adversarial"
+To test model on handcrafted adversarial input, run "python nb_test.py --handcrafted"
+To test model on basic naive-Bayes-targeted adversary, run "python nb_test.py --basic_adversary"
+    This adversary replaces the first word of each positive training review with one of the (randomly
+    selected) top-5 negative-leaning words (and vice versa for negative training reviews).
+
 To get most positive-leaning and most negative-leaning words, run 
     "python model_analysis.py --number k" where k is the number of top
     words you'd like to see. (e.g. "python model_analysis.py --number 5")
--- naive_bayes/nb_test.py
+++ naive_bayes/nb_test.py
@@ -3,6 +3,7 @@ from os import listdir
 from os.path import isfile, join
 import re
 import sys
+from random import randint
 
 # define for string cleaning
 strip_special_chars = re.compile("[^A-Za-z0-9 ]+")
@@ -29,9 +30,15 @@ def findIndex(search_list, begin, end, key):
         return findIndex(search_list, mid, end, key)
     return findIndex(search_list, begin, mid, key)
 
-adversarial_input = False
-if ('--adversarial' in sys.argv):
-    adversarial_input = True
+handcrafted_adversarial_input = False
+if ('--handcrafted' in sys.argv):
+    handcrafted_adversarial_input = True
+
+basic_naive_bayes_adversary = False
+if ('--basic_adversary' in sys.argv):
+    basic_naive_bayes_adversary = True
+bayes_top_positive = ['edie', 'antwone', 'din', 'gunga', 'yokai']
+bayes_top_negative = ['boll', '410', 'uwe', 'tashan', 'hobgoblins']
 
 # read in trained model
 nb_neg_weights = open("model_data/negative_weights.txt", "r")
@@ -49,10 +56,9 @@ nb_neg_weights.close()
 nb_pos_weights.close()
 
 # test on test data (not adversarial or adversarial)
-if not adversarial_input:
-    positiveFiles = ['../stanford_test/pos/' + f for f in listdir('../stanford_test/pos') if isfile(join('../stanford_test/pos/', f))]
-    negativeFiles = ['../stanford_test/neg/' + f for f in listdir('../stanford_test/neg') if isfile(join('../stanford_test/neg/', f))]
-else:
+positiveFiles = ['../stanford_test/pos/' + f for f in listdir('../stanford_test/pos') if isfile(join('../stanford_test/pos/', f))]
+negativeFiles = ['../stanford_test/neg/' + f for f in listdir('../stanford_test/neg') if isfile(join('../stanford_test/neg/', f))]
+if handcrafted_adversarial_input:
     positiveFiles = ['../adversarial_data/positiveReviews_test/' + f for f in listdir('../adversarial_data/positiveReviews_test') if isfile(join('../adversarial_data/positiveReviews_test/', f))]
     negativeFiles = ['../adversarial_data/negativeReviews_test/' + f for f in listdir('../adversarial_data/negativeReviews_test') if isfile(join('../adversarial_data/negativeReviews_test/', f))]
 
@@ -63,8 +69,10 @@ for pf in positiveFiles:
     positive_total = 0
     negative_total = 0
     with open(pf, "r") as f:
-        line = cleanSentences(f.readline())
-        for word in line.split():
+        line = cleanSentences(f.readline()).split()
+        if basic_naive_bayes_adversary:
+            line[0] = bayes_top_negative[randint(0, 4)]
+        for word in line:
             index = findIndex(wordsList, 0, len(wordsList)-1, word)
             if not index == -1:
                 positive_total += np.log(positive_weights[index])
@@ -77,8 +85,10 @@ for nf in negativeFiles:
     positive_total = 0
     negative_total = 0
     with open(nf, "r") as f:
-        line = cleanSentences(f.readline())
-        for word in line.split():
+        line = cleanSentences(f.readline()).split()
+        if basic_naive_bayes_adversary: 
+            line[0] = bayes_top_positive[randint(0, 4)]
+        for word in line:
             index = findIndex(wordsList, 0, len(wordsList)-1, word)
             if not index == -1:
                 positive_total += np.log(positive_weights[index])
