b2d05a2d51d31e95cfd8ef7011d1f0c087301ef3
Pulkit Tandon <pulkit@DN51s594.SUNet>
2017-12-11 09:48:38 -0800

    Added Code to Read text files and hence interactive statistics

--- lstm_adversarial/LSTM_Adversary_Stats.py
+++ lstm_adversarial/LSTM_Adversary_Stats.py
@@ -3,9 +3,9 @@ import os
 #Change Directory for Reading Files
 directory_in_str=str('/afs/cs.stanford.edu/u/aditir/scr/lstm/CS229-Project/lstm_adversarial')
 
-directory = os.fsencode(directory_in_str)
+directory = os.path.fsencode(directory_in_str)
 
-for file in os.listdir(directory):
+for file in os.path.listdir(directory):
     filename = os.fsdecode(file)
     if filename.endswith(".txt"): 
         print('.txt file found=>'+filename)
